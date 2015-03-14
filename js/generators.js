d51de5b4ff5025086d5d45db69e65b423c7111dc
shadowfiles <zhangtiff@gmail.com>
2015-03-13 22:48:52 -0400

    Integrated random startup name generator.

---
+++ js/generators.js
@@ -0,0 +1,46 @@
+function seedChoice (seed, choices) {
+    return choices[Math.floor(random(seed) * choices.length)];
+}
+
+var bgRoot = "img/bg/";
+
+function verb (seed) {
+    return seedChoice(seed, verbs);
+}
+
+function noun (seed) {
+    return seedChoice(seed, nouns);
+}
+
+function commonWord (seed) {
+    if (random(seed) > 0.5) {
+        return verb(seed);
+    } else {
+        return noun(seed);
+    }
+}
+
+function capitalizeFirst (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function getBackground (seed) {
+	return bgRoot + seedChoice(seed, backgrounds);
+}
+
+function startupify (seed) {
+    results = [];
+    results.push(commonWord(seed) + "r");
+    results.push(commonWord(seed) + "it");
+    results.push(commonWord(seed) + "ly");
+    results.push(commonWord(seed) + "ify");
+    results.push(commonWord(seed) + "hub");
+    results.push(commonWord(seed) + "y");
+    results.push(commonWord(seed));
+    results.push(commonWord(seed) + "me");
+    results.push("you" + commonWord(seed));
+    results.push(commonWord(seed) + "erific");
+    results.push(commonWord(seed) + "rific");
+    results.push(commonWord(seed) + "n");
+    return capitalizeFirst(seedChoice(seed + 1, results));
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+function random (seed) {
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+}
+
+var seed = Math.floor(Math.random() * 10000000000);
+
+$(document).ready(function(){ 
+	var bg = getBackground(seed);
+	var name = startupify(seed + 1);
+	$("#home").css("background-image", "url('" + bg + "')");
+	$("#logo").html(name);
+});
