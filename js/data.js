ebe2c17a40d3494e075a664a82f18322f2d8c773
shadowfiles <zhangtiff@gmail.com>
2015-03-14 01:24:38 -0400

    Added some more photos.

--- js/data.js
+++ js/data.js
@@ -6,7 +6,8 @@ var backgrounds = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg",
 										"26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg",
 										"31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", 
 										"36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg",
-										"41.jpg", "42.jpg", "43.jpg", "44.jpg"];
+										"41.jpg", "42.jpg", "43.jpg", "44.jpg", "45.jpg", 
+										"46.jpg", "47.jpg", "48.jpg", "49.jpg", "50.jpg"];
 										
 var mainColors = ["#1abc9c", "#16a085", "#f1c40f", "#f39c12", "#2ecc71", 
 									"#27ae60", "#e67e22", "#d35400", "#3498db", "#2980b9", 
