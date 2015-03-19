13e524c0a8f2b978ac08636bf8f907ba19f16a1a
shadowfiles <zhangtiff@gmail.com>
2015-03-19 01:04:07 -0400

    Added a bunch of new styles.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var seed = Math.floor(Math.random() * 10000000000);
 
 var accent;
+var navTop;
+var topColor = "#fff";
 
 $(document).ready(function() { 
 	var name = startupify(seed);
@@ -10,12 +12,41 @@ $(document).ready(function() {
   var shadowSize = randomInt(seed + 4, 300, 50);
   var slogan = makeSlogan(seed);
   var logo = getLogo(seed * 2);
+  var navColoredAtTop = seedChance(seed * 243 * 243, 0.3);
   accent = getColor(seed * 4);
+  navTop = setAlpha(accent, 0);
 
-  if (seedChance(seed * 243 * 9, 0.3)) {
+  $(".btn-default").css("background-color", accent);
+
+  if (navColoredAtTop) {
+  	var navAlpha = getAlpha(seed * 243 * 243 * 9, 0.8, 1);
+  	if (seedChance(seed * 243 * 243 * 3, 0.3)) {
+  		navTop = setAlpha(accent, navAlpha);
+  		topColor = "#ffffff";
+  	} else {
+  		navTop = setAlpha("#ffffff", navAlpha);
+  		topColor = darkenColor(accent, randomInt(seed * 243 * 243 * 27, 80), navAlpha);
+  	}
+  	$("#home-text-wrapper").css("margin-top", "30px")
+  } else if (seedChance(seed * 243 * 9, 0.6)) {
+  	var stripAlpha = getAlpha(seed * 243 * 243 * 3, 0, 0.4);
+  	$("#home-text-wrapper").css("background-color", setAlpha("#ffffff", stripAlpha));
+  	$("#home-text-wrapper").css("padding", "15px 0");
+  }
+
+  if (seedChance(seed * 243 * 9, 0.4)) {
   	// Generate a flat color
   	$("#home-content").css("background", accent);
+  	if (seedChance(seed * 243 * 81, 0.6)) {
+  		var bgAlpha = getAlpha(seed * 243 * 27, 0.4, 1);
+  		// Overlay a subtle background on top
+  		$("#home .before").css("background-image", "url('" + bg + "')");
+  		$("#home-content").css("background", setAlpha(accent, bgAlpha));
+  	}
+
+  	$("#home .btn-default").css("background-color", darkenColor(accent, 40));
   } else {
+  	// Generate an image
 	  var bgAlpha = getAlpha(seed * 243 * 27, 0.2, 0.6);
 	  var bgOverlay = "rgba(0, 0, 0, " + bgAlpha + ")";
 	  if (seedChance(seed * 3, 0.1)) {
@@ -40,6 +71,13 @@ $(document).ready(function() {
 	  $("#home .before").css("background-image", "url('" + bg + "')");
 	  $("#home").css("box-shadow", "inset 0 0 " + shadowSize + "px rgba(0,0,0, " + shadowAlpha + ")");
 	  $("#home-content").css("background", bgOverlay);
+
+	  if (!navColoredAtTop && seedChance(seed * 243 * 243 * 81, 0.3)) {
+	  	var stripAlpha = getAlpha(seed * 243 * 243 * 3, 0.2, 0.7);
+	  	$("#home-text-wrapper").css("background-color", setAlpha("#000000", stripAlpha));
+	  	$("#home-text-wrapper").css("padding", "15px 0");
+	  }
+
 	}
 
 
--- js/style.js
+++ js/style.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
    var startChange = $('#cover-heading');
    var offset = startChange.offset();
 
-   $(".btn-default").css("background-color", setAlpha(accent, 1));
-
    function checkScroll() { 
     scrollStart = $(this).scrollTop();
     if(scrollStart > offset.top) {
         $("#navigation").removeClass("top");
         $("#navigation").addClass("scrolled");
         $("#navigation").css("background-color", setAlpha(accent, 1));
+        $("#navigation a").css("color", "#fff");
      } else {
         $("#navigation").removeClass("scrolled");
         $("#navigation").addClass("top");
-        $("#navigation").css("background-color", setAlpha(accent, 0));
+        $("#navigation").css("background-color", navTop);
+        $("#navigation a").css("color", topColor);
      }
    }
 
@@ -30,4 +30,15 @@ function setAlpha(color, alpha) {
           + parseInt(color.slice(-4,-2),16) + ',' 
           + parseInt(color.slice(-2),16) + ', ' 
           + alpha + ')';
+}
+
+function darkenColor(color, amount, alpha) {
+  if (!alpha) alpha = 1;
+  return 'rgba(' + zeroFloor(parseInt(color.slice(-6,-4),16) - amount) + ',' 
+          + zeroFloor(parseInt(color.slice(-4,-2),16) - amount) + ',' 
+          + zeroFloor(parseInt(color.slice(-2),16) - amount) + ', ' + alpha + ')';
+}
+
+function zeroFloor(num) {
+  return Math.max(num, 0);
 }
